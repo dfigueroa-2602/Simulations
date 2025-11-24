1d885ec2a973c1918b73beafe7b16e32fbd2d03f
Dave Figueroa <dave.figueroa.dokt@pw.edu.pl>
2025-11-24 11:23:04 +0100

    VSI_Ccode v0.1
    Creating the simulation

---
+++ VSI/VSI_Ccode/MATLAB/VSI_C_Init.m
@@ -0,0 +1,24 @@
+clc;
+Lf = 3e-3; Cf = 12e-6; Rf = 350e-3; RL = 47;
+fsw = 5e3; Ts = 1/fsw;
+
+VDC = 540; f = 50; w = 2*pi*f;
+
+A = [-Rf/Lf  w      -1/Lf  0    ;
+     -w     -Rf/Lf  0     -1/Lf ;
+      1/Cf   0      0      w    ;
+      0      1/Cf  -w      0];
+B = [1/Lf   0    ;
+      0     1/Lf ;
+      0     0    ;
+      0     0   ];
+P = [ 0     0    ;
+      0     0    ;
+     -1/Cf  0    ;
+      0    -1/Cf];
+nx = size(B,1); nu = size(B,2);
+C = eye(nx); D = zeros(nx,nu);
+
+ss_VSI = ss(A,B,C,D); [Ad,Bd,~,~] = ssdata(c2d(ss_VSI,Ts));
+
+sim('VSI_C_Simulation_Blocks');
