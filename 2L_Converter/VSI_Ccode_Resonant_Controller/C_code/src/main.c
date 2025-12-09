5500bf7fa1342013bdb2e0c8f11c2854fd02c439
Dave Figueroa <dave.figueroa.dokt@pw.edu.pl>
2025-12-09 16:14:30 +0100

    VSI_Ccode Resonant v0.7
    Final Version

---
+++ 2L_Converter/VSI_Ccode_Resonant_Controller/C_code/src/main.c
@@ -0,0 +1,120 @@
+#include "stdafx.h"
+#include "stddef.h"
+#include "Control.h"
+#include "Converter.h"
+
+DLLEXPORT void plecsSetSizes(struct SimulationSizes* aSizes)
+{
+    aSizes->numInputs     = 9; /* DLL inputs */
+    aSizes->numOutputs    = 4;  /* control outputs u */
+    aSizes->numStates     = 0;  /* discrete states inside DLL. Increase when you want to integrate something inside */
+    aSizes->numParameters = 1;  /* user parameters that will be used inside the DLL */
+}
+
+/* Called once at the beginning of a simulation */
+DLLEXPORT void plecsStart(struct SimulationState* aState)
+{
+    aState_global = aState;
+
+    /* Initialization of the integrators initial constants*/
+    memset(&var_Control_struct, 0, sizeof(var_Control_struct));
+
+    /* Initialize outputs to 0 */
+    for (int i = 0; i < 3; ++i)
+        aState->outputs[i] = 0.0;
+}
+
+/* Called at each sample time */
+DLLEXPORT void plecsOutput(struct SimulationState* aState)
+{   
+    /* Read inputs */
+    Meas.Vdc        = aState->inputs[0];
+    Meas.is.a       = (float)aState->inputs[1];
+    Meas.is.b       = (float)aState->inputs[2];
+    Meas.is.c       = (float)aState->inputs[3];
+    Meas.vc.a       = (float)aState->inputs[4];
+    Meas.vc.b       = (float)aState->inputs[5];
+    Meas.vc.c       = (float)aState->inputs[6];
+    float vcalpha_ref   = (float)aState->inputs[7];
+    float vcbeta_ref   = (float)aState->inputs[8];
+
+    /* Sample time from parameters */
+    double Ts = aState->parameters[0];
+
+    /* Build needed structs */
+    struct alphabeta_struct if_ab;
+    struct alphabeta_struct vc_ab;
+    struct alphabeta_struct e_k_ab;
+    struct dq_struct        if_dq;
+    struct dq_struct        vc_dq;
+
+    /* Clarke Transform */
+    abc_alphabeta(if_ab, Meas.is);
+    abc_alphabeta(vc_ab, Meas.vc);
+
+    /* Error Calculation */
+    e_k_ab.alpha = vcalpha_ref - vc_ab.alpha;
+    e_k_ab.beta = vcbeta_ref - vc_ab.beta;
+
+    var_Control_struct.States.e[0] = e_k_ab.alpha;
+    var_Control_struct.States.e[1] = e_k_ab.beta;
+
+    resonantSystem(var_Control_struct.States.e, var_Control_struct.States.rho_k, var_Control_struct.States.rho_k_1);
+
+    double x[10]; double u[2];
+
+    /* x = [ifalpha ifbeta vcalpha vcbeta vsalpha_k vsbeta_k rho_0 rho_1 rho_2 rho_3]*/
+
+    x[0] = (double)if_ab.alpha;
+    x[1] = (double)if_ab.beta;
+    x[2] = (double)vc_ab.alpha;
+    x[3] = (double)vc_ab.beta;
+    x[4] = var_Control_struct.States.u_k[0];
+    x[5] = var_Control_struct.States.u_k[1];
+    x[6] = var_Control_struct.States.rho_k[0];
+    x[7] = var_Control_struct.States.rho_k[1];
+    x[8] = var_Control_struct.States.rho_k[2];
+    x[9] = var_Control_struct.States.rho_k[3];
+
+    /* Compute LQR control */
+    lqrControl(x, u);
+
+    var_Control_struct.Output.m_k_alphabeta.alpha = u[0];
+    var_Control_struct.Output.m_k_alphabeta.beta = u[1];
+
+    float m_alpha = var_Control_struct.Output.m_k_alphabeta.alpha;
+    float m_beta  = var_Control_struct.Output.m_k_alphabeta.beta;
+
+    float m_sat = 540.0f;
+    if (m_alpha >  m_sat) m_alpha =  m_sat;
+    if (m_alpha < -m_sat) m_alpha = -m_sat;
+    if (m_beta  >  m_sat) m_beta  =  m_sat;
+    if (m_beta  < -m_sat) m_beta  = -m_sat;
+
+    var_Control_struct.States.u_k[0] = m_alpha;
+    var_Control_struct.States.u_k[1] = m_beta;
+
+    var_Control_struct.Output.u_alphabeta.alpha = var_Control_struct.States.u_k[0];
+    var_Control_struct.Output.u_alphabeta.beta = var_Control_struct.States.u_k[1];
+
+    alphabeta_abc(var_Control_struct.Output.u_abc, var_Control_struct.Output.u_alphabeta);
+
+    /* With the given actuaction, create the duty cycle that will be given to the 2L inverter*/
+    Converter_2L_minmax();
+    
+    /* Assign the duty cycle to the outputs */
+    aState_global->outputs[0] = conv.duty.a;
+    aState_global->outputs[1] = conv.duty.b;
+    aState_global->outputs[2] = conv.duty.c;
+
+    aState_global->outputs[3] = var_Control_struct.States.u_k[0];
+
+    /* Store updated integrator states */
+    variablesUpdate();
+}
+
+/* Called once at end of simulation */
+DLLEXPORT void plecsTerminate(struct SimulationState* aState)
+{
+    (void)aState;
+}
---
+++ 2L_Converter/VSI_Ccode_Resonant_Controller/MATLAB/Init_VSI_Resonant.m
@@ -0,0 +1,103 @@
+clc;
+Lf = 3e-3; Cf = 12e-6; Rf = 35e-3; RL = 47;
+fsw = 5e3; Ts = 1/fsw;
+
+VDC = 540; f = 50; w = 2*pi*f;
+
+A = [-Rf/Lf  0     -1/Lf   0    ;
+      0     -Rf/Lf  0     -1/Lf ;
+      1/Cf   0      0      0    ;
+      0      1/Cf   0      0   ];
+
+B = [1/Lf   0    ;
+      0     1/Lf ;
+      0     0    ;
+      0     0   ];
+
+P = [ 0     0    ;
+      0     0    ;
+     -1/Cf  0    ;
+      0    -1/Cf];
+
+nx = size(B,1); nu = size(B,2);
+C = eye(nx); D = zeros(nx,nu);
+
+ss_VSI = ss(A,B,C,D); [Ad,Bd,~,~] = ssdata(c2d(ss_VSI,Ts,'zoh'));
+
+Ad_delay = [Ad Bd; zeros(nu,nx) zeros(nu)]; Bd_delay = [zeros(nx,nu); eye(nu)];
+
+Ar1 = [0 w; -w 0];
+Br1 = [1; 0];
+
+ss_res1 = ss(Ar1,Br1,eye(2),[]); [Ard1,Brd1,~,~] = ssdata(c2d(ss_res1,Ts,'tustin'));
+
+Ard = blkdiag(Ard1,Ard1);
+Brd = blkdiag(Brd1,Brd1);
+
+nxr = size(Brd,1); nur = size(Brd,2);
+
+Ha = [0 0 1 0 ;
+      0 0 0 1];
+
+C_delay = [C zeros(nx,nu)];
+
+Hx = C_delay([3 4],:);
+
+Ad_aug = [Ad_delay zeros((nx+nu),nxr); -Brd*Hx Ard];
+Bd_aug = [Bd_delay; zeros(nxr,nu)];
+
+Tsim = 0.3; A_ref = 60; T_settling = 40e-3;
+
+search = 1;
+
+beta_c = 3e-5; n_part = 100; iter = 100;
+c1 = 2.05; c2 = 2.05;
+Qmax = 4; Rmax = 2; xi_Max = 0.2; xivel_max = 0.01; Qmax_vel = 1; Rmax_vel = 0.1; rang_coef = 0.6;
+
+nQ = size(Bd_aug,1)/2; nR = size(Bd_aug,2); dim = nQ;
+
+[vel_clamp,Kap,swarm,space_range] = Swarm_Init(n_part,0,0,nQ,nR,c1,c2,Qmax,Rmax,xi_Max,rang_coef,Qmax_vel,Rmax_vel,xivel_max);
+xmax_val = 10; xmin = repmat(-xmax_val,dim,1); xmax = repmat(xmax_val,dim,1);
+
+% aux = swarm(10,:,:);
+% LQR_Search(Ts,0,0,model,squeeze(aux(1,1,:)),Ad_aug,Bd_aug,nx,nu,nxr,beta_c)
+
+rng(1,'twister');
+if search == 1
+    swarms = {};
+    % Randomize the seed, then open a pool of workers
+    rng('shuffle'); if isempty(gcp('nocreate')), parpool; end
+
+    % Initialization of important matrices
+    b_fitness = zeros(iter,1); fitness = inf(1,n_part);
+    gbest_vec = zeros(iter,1);
+    % Initialize particles
+    for i = 1:iter
+        parfor n = 1:n_part
+            try
+                sw = swarm(n,:,:);
+                sw = squeeze(sw(1,1,:));
+                fitness(n) = LQR_Search(Ts,0,sw,Ad_aug,Bd_aug, ...
+                    Ad,Bd,Ard,Brd,Ha,beta_c,VDC,w,A_ref,Tsim,T_settling);
+            catch
+                fitness(n) = 1e6;
+                disp(['Evaluation for particle no. ' num2str(n) ' was aborted']);
+            end
+        end
+        for n = 1:n_part
+            if fitness(n) < swarm(n,4,1)
+                swarm(n,3,:) = swarm(n,1,:);
+                swarm(n,4,1) = fitness(n);
+            end
+        end
+        PSO_Algorithm;
+    end
+
+    [Kx, Kr, Ku] = Final_Value(b_swarm,Ad_aug,Bd_aug,nx,nu,nxr,0);
+
+    delete(gcp('nocreate'));
+end
+
+Kgain2Ccode({[Kx Ku Kr] Ard, Brd}, ...
+            {'K_LQR_Values','Ard_Values','Brd_Values'}, ...
+            'Matrices')
